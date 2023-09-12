2338389d52f64bdc3de5aca7862e8aba5dcdb315
TDBarnhouse <td.barnhouse@yahoo.com>
2023-09-11 20:17:43 -0400

    Updated portfolio, navbar, and alternating title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let characterIndex = 0;
 
-let titleNames = ["t", "tr", "tri", "tris", "trist", "trista", "tristan", "trista", "trist", "tris", "tri", "tr", "t"];
-
+let titleNames = [
+    "t", "tr", "tri", "tris", "trist", "trista", "tristan", "tristan.", "tristan.b", "tristan.ba", "tristan.bar", 
+    "tristan.barn", "tristan.barnh", "tristan.barnho", "tristan.barnhou", "tristan.barnhous", "tristan.barnhouse", 
+    "tristan.barnhous", "tristan.barnhou", "tristan.barnho", "tristan.barnh", "tristan.barn", "tristan.bar", "tristan.ba", 
+    "tristan.b", "tristan.", "tristan", "trista", "trist", "tris", "tri", "tr", "t"
+];
 window.updateTitleName = function () {
     document.getElementsByTagName("title")[0].innerHTML = titleNames[characterIndex++ % titleNames.length];
 }
@@ -14,10 +18,10 @@ $(document).ready(function(){
     let currentPage = "HOME";
     let currentNavLink = $("#navbar-menu").children().first().children()[0];
 
-    $("#TERMINAL").hide();
     $("#ABOUT").hide();
     $("#PORTFOLIO").hide();
     $("#CONTACT").hide();
+    $("#TERMINAL").hide();
 
     $(".navbar-link").click(function(e){
         $(`#${currentPage}`).fadeOut(100)
