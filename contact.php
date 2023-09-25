fdacc1e64a948b78acd844aa6f6276b5cb4d1b7d
TDBarnhouse <td.barnhouse@yahoo.com>
2023-09-25 19:47:37 -0400

    Updated contact.php

--- contact.php
+++ contact.php
@@ -1,24 +1,27 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $subject = "Contact Form Submission";
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $number = $_POST["number"];
-    $message = $_POST["message"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $subject = "Contact Form Submission";
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $number = $_POST["number"];
+        $message = $_POST["message"];
 
-    $to = "youremail@example.com"; // Replace with your email address
+        $to = "itsbeastieboy@gmail.com";
+        $headers = "From: tristanbarnhouse.dev@noreply.com\r\n";
+        // $headers .= "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
 
-    // Build the email message
-    $message_body = "Name: " . $name . "\n";
-    $message_body .= "Email: " . $email . "\n";
-    $message_body .= "Phone Number: " . $number . "\n";
-    $message_body .= "Message:\n" . $message;
+        $message_body = "<b>Name:</b> " . $name . "<br>";
+        $message_body .= "<b>Email:</b> " . $email . "<br>";
+        $message_body .= "<b>Phone Number:</b> " . $number . "<br>";
+        $message_body .= "<b>Message:</b><br>" . $message;
 
-    // Send the email
-    if (mail($to, $subject, $message_body)) {
-        echo "<p>Your message has been sent successfully.</p>";
-    } else {
-        echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
+        if (mail($to, $subject, $message_body, $headers)) {
+            echo "<p>Your message has been sent successfully.</p>";
+        } else {
+            echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
+        }
     }
-}
+
+    // Make it so it stays on the current page but clears all fields with maybe a popup below the submit button stating success for message sent
 ?>
